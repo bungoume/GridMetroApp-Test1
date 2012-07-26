30b6e26f2b8b7a73299f3e87bccb696412ce0cc9
bungoume <bungoume3@yahoo.co.jp>
2012-07-26 15:29:40 +0900

    first commit

---
+++ GridMetroApp-Test1/js/default.js
@@ -0,0 +1,44 @@
+﻿// グリッド テンプレートの概要については、次のドキュメントを参照してください:
+// http://go.microsoft.com/fwlink/?LinkID=232446
+(function () {
+    "use strict";
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+    var nav = WinJS.Navigation;
+    WinJS.strictProcessing();
+
+    app.addEventListener("activated", function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: このアプリケーションは新しく起動しました。ここでアプリケーションを
+                // 初期化します。
+            } else {
+                // TODO: このアプリケーションは中断状態から再度アクティブ化されました。
+                // ここでアプリケーションの状態を復元します。
+            }
+
+            if (app.sessionState.history) {
+                nav.history = app.sessionState.history;
+            }
+            args.setPromise(WinJS.UI.processAll().then(function () {
+                if (nav.location) {
+                    nav.history.current.initialPlaceholder = true;
+                    return nav.navigate(nav.location, nav.state);
+                } else {
+                    return nav.navigate(Application.navigator.home);
+                }
+            }));
+        }
+    });
+
+    app.oncheckpoint = function (args) {
+        // TODO: このアプリケーションは中断しようとしています。ここで中断中に
+        // that needs to persist across suspensions here. If you need to 
+        // complete an asynchronous operation before your application is 
+        // suspended, call args.setPromise().
+        app.sessionState.history = nav.history;
+    };
+
+    app.start();
+})();
